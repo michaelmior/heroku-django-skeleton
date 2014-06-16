fb56a663fc11edc7a19f0f7339731f5a55d44f94
Michael Mior <mmior@uwaterloo.ca>
2014-06-16 11:18:06 -0400

    Use dj-static for static files

--- app/wsgi.py
+++ app/wsgi.py
@@ -22,7 +22,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
